e9d1594a1383c9b8a3e69b6305542a6d7a15912a
Prabhakar Kumar <prabhakarrai.kumar31@gmail.com>
2024-09-21 23:19:38 +0530

    Error resolved

--- src/main/java/com/localityconnector/models/Vendor.java
+++ src/main/java/com/localityconnector/models/Vendor.java
@@ -1,11 +1,11 @@
 package com.localityconnector.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 
 @Entity
 @Table(name = "vendors")
